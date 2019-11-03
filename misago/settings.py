50e6a900a0b9e25f0e61914781844682c7248086
Lance Moore <lance@adminout.net>
2019-11-03 23:15:58 +0200

    Remove S3

--- misago/settings.py
+++ misago/settings.py
@@ -405,18 +405,4 @@ MISAGO_PROFILE_FIELDS = [
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 
-INSTALLED_APPS += ["gunicorn", "storages"]
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-# http://django-storages.readthedocs.org/en/latest/index.html
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_ACCESS_KEY_ID = os.getenv("DJANGO_AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("DJANGO_AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.getenv("DJANGO_AWS_STORAGE_BUCKET_NAME")
-AWS_DEFAULT_ACL = "public-read"
-AWS_AUTO_CREATE_BUCKET = True
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_REGION_NAME = os.getenv("DJANGO_AWS_S3_REGION_NAME")
-MEDIA_URL = f"https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/"
+INSTALLED_APPS += ["gunicorn"]
