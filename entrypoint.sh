4420b61015a0a947f2e7997457b268d7becdd5a8
Lance Moore <lance@adminout.net>
2019-11-03 20:28:35 +0200

    don't wait for entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-while ! nc -w 1 -z "${POSTGRES_HOST}" 5432; do
-    >&2 echo "Postgres is unavailable - sleeping"
-    sleep 1;
-done;
->&2 echo "Postgres is up - continuing..."
-
 if [ "x$ENVIRON_NAME" = 'xproduction' ]; then
  echo "Some house keeping: collecting static files and migrating databases" &&
   python manage.py migrate --noinput &&
