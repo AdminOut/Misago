07750572172099c56586add7689cda8e0ad646c4
Lance Moore <lmoore@engineerroi.com>
2020-04-06 12:26:53 +0200

    Seperate medcube app

---
+++ medcube/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MedcubeConfig(AppConfig):
+    name = 'medcube'
--- medcube/settings.py
+++ medcube/settings.py
@@ -420,4 +420,4 @@ if USE_S3:
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
-INSTALLED_APPS += ["gunicorn"]
+INSTALLED_APPS += ["gunicorn", "medcube"]
